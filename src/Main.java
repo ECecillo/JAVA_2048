517105d1973ca8eca5ad53c116533b0f4122c3a9
OUAR BADREDDINE p1801885 <badreddine.ouar@etu.univ-lyon1.fr>
2022-04-27 09:45:12 +0000

    Replace Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,19 +18,27 @@ public class Main {
         Console2048 vue = new Console2048(jeu);
         jeu.addObserver(vue);
 
+
         vue.start();
 
     }
 
     public static void mainSwing() {
 
-        Jeu jeu = new Jeu(4);
-        Swing2048 vue = new Swing2048(jeu);
-        // 
-        jeu.addObserver(vue);
-        // On affiche.
-        vue.setVisible(true);
+        Jeu jeu1 = new Jeu(4);
+        Jeu jeu2 = new Jeu(4);
+        //Jeu jeu2 = jeu;
+        Swing2048 vue1 = new Swing2048(jeu1, 50, 50);
+        Swing2048 vue2 = new Swing2048(jeu2, 300, 50);
+        //
 
+        //Swing2048 vue = new Swing2048(jeu);
+
+        jeu1.addObserver(vue1);
+        jeu2.addObserver(vue2);
+        // On affiche.
+        vue1.setVisible(true);
+        vue2.setVisible(true);
 
     }
 
