f397b4fed646c3e69dc68f9e649197aeec3113bc
OUAR BADREDDINE p1801885 <badreddine.ouar@etu.univ-lyon1.fr>
2022-04-27 09:47:43 +0000

    Replace Swing2048.java

--- src/vue_controleur/Swing2048.java
+++ src/vue_controleur/Swing2048.java
@@ -20,11 +20,11 @@ import java.util.Observer;
 public class Swing2048 extends JFrame implements Observer {
     private static final int PIXEL_PER_SQUARE = 60;
     // tableau de cases : i, j -> case graphique
-    private JLabel[][] tabC1, tabC2; //On déclare deux tableaux prochainement identiques pour créer les deux JPanels
+    private JLabel[][] tabC; //On déclare un tableau pour créer le JPanel
     private Jeu jeu;
     boolean initializer;
 
-    public Swing2048(Jeu _jeu) {
+    public Swing2048(Jeu _jeu, int _x, int _y) {
         jeu = _jeu;
 
         //initializer = true;
@@ -32,26 +32,24 @@ public class Swing2048 extends JFrame implements Observer {
         // Fonction qui s'occupe de terminer la fenêtre lorsque l'on appuie sur un bouton fermer.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Définie la taille de la JFrame en fonction de la taille de la fenêtre ().
-        setSize(jeu.getSize() * PIXEL_PER_SQUARE * 2 + 100, jeu.getSize() * PIXEL_PER_SQUARE);
+        setSize(jeu.getSize() * PIXEL_PER_SQUARE, jeu.getSize() * PIXEL_PER_SQUARE);
         //pack();
 
-        //setLocation(_x, _y);
+        setLocation(_x, _y);
 
         // On récupère le panel par défaut et on change sa définition
-        JPanel panel = (JPanel)this.getContentPane();
+        //JPanel panel = (JPanel)this.getContentPane();
         // On le présente sous la forme d'un tableau 1, 2
-        panel.setLayout(new GridLayout(1, 2));
+        //panel.setLayout(new GridLayout(1, 2));
 
         // JLabel : Composant permettant d'afficher du texte ou une image.
         // On alloue dans la mémoire un tableau 2D de Type JLabel.
-        // On applique les mêmes traitements aux deux tableau sémantiquement différents
-        tabC1 = new JLabel[jeu.getSize()][jeu.getSize()];
-        tabC2 = new JLabel[jeu.getSize()][jeu.getSize()];
+        tabC = new JLabel[jeu.getSize()][jeu.getSize()];
 
         // Un Pane est associé à un JFrame, ce dernier est soit un contentPane soit un menuPane.
         // Un pane sera a son tour affecté à un Layout Manager pour dire comment le Pane se comporte dans la JFrame.
-        JPanel panel1 = new JPanel(new GridLayout(jeu.getSize(), jeu.getSize()));
-        JPanel panel2 = new JPanel(new GridLayout(jeu.getSize(), jeu.getSize()));
+        JPanel panel = new JPanel(new GridLayout(jeu.getSize(), jeu.getSize()));
+        //JPanel panel2 = new JPanel(new GridLayout(jeu.getSize(), jeu.getSize()));
         //JPanel panel3 = new JPanel(new GridLayout(jeu.getSize(), jeu.getSize()));
         // Défini le placement des fils en gridLayout.
 
@@ -64,27 +62,19 @@ public class Swing2048 extends JFrame implements Observer {
                 // Factory : Design patern qui permet de créer un nouvelle objet à partir d'une interface (ici BorderFactory)
                 Border border = BorderFactory.createLineBorder(Color.darkGray, 5);
                 // Dans chaque case du tableau on stock on composant JLabel.
-                tabC1[i][j] = new JLabel();
-                tabC2[i][j] = new JLabel();
+                tabC[i][j] = new JLabel();
                 // On défini pour ce composant une bordure avec setBorder.
-                tabC1[i][j].setBorder(border);
-                tabC2[i][j].setBorder(border);
+                tabC[i][j].setBorder(border);
                 // On définie l'alignement du composant pour qu'il soit au centre. 
-                tabC1[i][j].setHorizontalAlignment(SwingConstants.CENTER);
-                tabC2[i][j].setHorizontalAlignment(SwingConstants.CENTER);
-
-
-                // On affecte respectivement les tableaux différents aux Panels différents
-                panel1.add(tabC1[i][j]);
-                panel2.add(tabC2[i][j]);
+                tabC[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+                
+                panel.add(tabC[i][j]);
                 //panel3.add(tabC[i][j]);
 
             }
         }
         // On remplace le ContentPane par notre Panel pane que l'on vient de créer.
         setContentPane(panel);
-        panel.add(panel1);
-        panel.add(panel2);
         //panel.add(panel3);
         //setContentPane(panel);
         ajouterEcouteurClavier();
@@ -108,12 +98,10 @@ public class Swing2048 extends JFrame implements Observer {
                         // Si la case du tableau est null on affiche une case avec un texte vide.
                         if (c == null || c.getValeur() == 0) {
 
-                            tabC1[i][j].setText("");
-                            tabC2[i][j].setText("");
+                            tabC[i][j].setText("");
 
                         } else {
-                            tabC1[i][j].setText(c.getValeur() + "");
-                            tabC2[i][j].setText(c.getValeur() + ""); // On lui met une valeur 2 , 4 ....
+                            tabC[i][j].setText(c.getValeur() + ""); // On lui met une valeur 2 , 4 ....
                         }
 
 
